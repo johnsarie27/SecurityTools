9024cc6167acb90ff58cdd9afe04cb2332543224
Justin Johns <just9539@esri.com>
2019-06-12 12:56:09 -0700

    Update added BaselinePath param added logic for baseline

--- Export-SQLVAReport.ps1
+++ Export-SQLVAReport.ps1
@@ -7,16 +7,21 @@ function Export-SQLVAReport {
         in SQL Server Management Studio 17.0 & up
     .PARAMETER ServerName
         Name of SQL Server
+    .PARAMETER BaselinePath
+        Path to folder containing baseline JSON file(s). The JSON file names must
+        match the DB server names.
     .PARAMETER DestinationPath
         Path to output directory for new SQL Vulnerability Assessment reports
+    .PARAMETER PassThru
+        Returns path to directory containing new reports
     .INPUTS
         System.String.
     .OUTPUTS
         System.String.
     .EXAMPLE
-        PS C:\> .\Export-SQLVAReport.ps1 -CP C:\config.json
-        This exports out a scan report using SQL Vulnerability Assessment tool for
-        each database in the provided configuraiton file.
+        PS C:\> Export-SQLVAReport.ps1 -SN MySQLServer -BP C:\Baselines -DP C:\Reports
+        This exports a scan report using SQL Vulnerability Assessment tool for the master
+        database on MySQLServer using the corresponding baseline file in C:\Baselines.
     .NOTES
         General notes
         https://docs.microsoft.com/en-us/sql/relational-databases/security/sql-vulnerability-assessment?view=sql-server-2017
@@ -28,9 +33,14 @@ function Export-SQLVAReport {
         [Alias('SN', 'Server')]
         [string[]] $ServerName,
 
+        [Parameter(HelpMessage = 'Path to folder containing baseline JSON file(s)')]
+        [ValidateScript({ Get-ChildItem -Path $_ -Filter *.json })]
+        [Alias('BP', 'Baseline')]
+        [string] $BaselinePath,
+
         [Parameter(HelpMessage = 'Output directory')]
         [ValidateScript( { Test-Path -Path $_ -PathType Container })]
-        [Alias('OP', 'Output')]
+        [Alias('DP', 'Output', 'Destination')]
         [string] $DestinationPath = "D:\MSSQL-VA",
 
         [Parameter(HelpMessage = 'Return path to report directory')]
@@ -51,6 +61,9 @@ function Export-SQLVAReport {
             try { New-Item -Path $Folder -ItemType Directory }
             catch { Write-Error ('Could not create folder path: [{0}]' -f $Folder) }
         }
+
+        # CREATE SPLATTER TABLE
+        $SVAParams = @{ DatabaseName = "master" }
     }
 
     Process {
@@ -60,8 +73,26 @@ function Export-SQLVAReport {
             # CREATE FILE
             $ReportFile = '{0}_{1}.xlsx' -f (Get-Date -F "yyyy-MM-dd"), $Server
 
+            # UPDATE PARAMETERS
+            $SVAParams['ServerInstance'] = $Server
+
+            # CHECK FOR BASELINE PARAM
+            if ( $PSBoundParameters.ContainsKey('BaselinePath') ) {
+                # GET BASELINE FILE
+                $BLFile = (Get-ChildItem -Path $BaselinePath -Filter "$Server.json").FullName
+
+                # CONVERT JSON FILE TO BASELINE OBJECT
+                if ($BLFile) {
+                    $Baseline = Import-SqlVulnerabilityAssessmentBaselineSet -FolderPath $BLFile
+                    $SVAParams['Baseline'] = $Baseline
+                }
+                else {
+                    Write-Warning ('Baseline file not found for server: [{0}]' -f $Server)
+                }
+            }
+
             # EXECUTE SCAN AND SET TO VARIABLE
-            $Scan = Invoke-SqlVulnerabilityAssessmentScan -ServerInstance $Server -DatabaseName "master"
+            $Scan = Invoke-SqlVulnerabilityAssessmentScan @SVAParams
 
             # EXPORT SCAN RESULT TO EXCEL SPREADSHEET
             $Scan | Export-SqlVulnerabilityAssessmentScan -FolderPath (Join-Path -Path $Folder -ChildPath $ReportFile)
