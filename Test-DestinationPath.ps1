2fa70349a854ca3a89c96974c1081bf125d532dc
Justin Johns <johnsarie@gmail.com>
2019-07-04 11:16:29 -0700

    Update refactored function

--- Test-DestinationPath.ps1
+++ Test-DestinationPath.ps1
@@ -1,9 +1,9 @@
 function Test-DestinationPath {
     <# =========================================================================
     .SYNOPSIS
-        Validate path
+        Test for a proper destination path
     .DESCRIPTION
-        Validate path meets use cases. See Pester tests file for user cases.
+        Test for a proper destination path
     .PARAMETER Path
         Path to validate
     .PARAMETER Extension
@@ -14,7 +14,7 @@ function Test-DestinationPath {
         System.String.
     .EXAMPLE
         PS C:\> Test-DestinationPath -Path C:\Users\JSmith\TestFolder\file.xml
-        Returns a "True" value if path can be used
+        Returns path if path can be used as destination path
     .NOTES
         General notes
     ========================================================================= #>
@@ -33,10 +33,14 @@ function Test-DestinationPath {
         ValueFromPipeline=$false,
         ValueFromPipelineByPropertyName=$false)]
         [ValidateNotNullOrEmpty()]
+        [ValidatePattern('\.\w+')]
         [string] $Extension,
 
-        [Parameter(Mandatory=$false, ValueFromPipeline=$false, ValueFromPipelineByPropertyName=$false)]
-        [switch] $Force=$false
+        [Parameter(Mandatory=$false,
+        ValueFromPipeline=$false,
+        ValueFromPipelineByPropertyName=$false)]
+        [Alias('AO')]
+        [switch] $AllowOverwrite
     )
     Begin {
         # ENSURE THE DESTINATION PATH IS IN A NON-PS-SPECIFIC FORMAT
@@ -45,20 +49,19 @@ function Test-DestinationPath {
 
         # VALIDATE PARENT PATH
         $parentDirectory = [System.IO.Path]::GetDirectoryName($Path)
-        if ($null -eq $parentDirectory) {
-            Throw 'Invalid file path'
-        }
-        if ($parentDirectory -eq [string]::Empty) {
-            $parentDirectory = '.'
-        }
-        if (-not (Test-Path -Path $parentDirectory -PathType Container)) {
-            Throw 'Invalid file path. Parent directory does not exist'
-        }
         Write-Verbose ('parent directory: {0}' -f $parentDirectory)
+        
+        if ($null -eq $parentDirectory) { Throw 'Invalid file path' }
+        if ($parentDirectory -eq [string]::Empty) { $parentDirectory = '.' }
 
+        if (-not (Test-Path -Path $parentDirectory -PathType Container)) {
+            Throw ('All or part of the directory "{0}" does not exist' -f $parentDirectory)
+        }
+        
         # JOIN FILE NAME AND PARENT PATH
         $fileName = [System.IO.Path]::GetFileName($Path)
         Write-Verbose ('file name: {0}' -f $fileName)
+        
         $Path = Join-Path -Path $parentDirectory -ChildPath $fileName
         Write-Verbose ('path: {0}' -f $Path)
 
@@ -66,6 +69,10 @@ function Test-DestinationPath {
         $fileExtension = [system.IO.Path]::GetExtension($Path)
         Write-Verbose ('file extension: {0}' -f $fileExtension)
         if ($fileExtension -eq [string]::Empty) {
+            # CHECK FOR EXISTING FOLDER WITH SAME NAME
+            if (Test-Path -Path $Path -PathType Container) {
+                Write-Warning 'New file will be created with same name as existing folder'
+            }
             $Path = $Path + $Extension
             Write-Verbose ('Added extension [{0}] to path' -f $Extension)
         }
@@ -75,14 +82,15 @@ function Test-DestinationPath {
 
         # CHECK IF FILE EXISTS OR IS READ ONLY
         $fileExist = Test-Path -LiteralPath $Path -PathType Leaf
-        if ($fileExist -and $Force -eq $false) {
-            Throw 'File exists. Use -Force to overwrite file.'
+        if ($fileExist -and -not $PSBoundParameters.ContainsKey('AllowOverwrite')) {
+            Throw 'File exists. Use -AllowOverwrite to overwrite file.'
         }
-        if ($fileExist -and $Force -eq $true) {
+        if ($fileExist -and $PSBoundParameters.ContainsKey('AllowOverwrite')) {
             $item = Get-Item -Path $Path
             if ($item.Attributes.ToString().Contains("ReadOnly")) {
                 Throw 'File is read only'
             }
+            Write-Warning 'File exists and will be overwritten.'
         }
     }
     End {
