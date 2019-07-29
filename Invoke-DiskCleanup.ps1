32ec9d80fd5fdc03ec72bb93c50da66688329953
Justin Johns <johnsarie@gmail.com>
2019-07-28 18:33:45 -0700

    Update added comments for future work

--- Invoke-DiskCleanup.ps1
+++ Invoke-DiskCleanup.ps1
@@ -5,22 +5,37 @@
 # Version:      0.0.1
 # ==============================================================================
 
-<#
+<# =========================================================================
+.SYNOPSIS
+    Short description
+.DESCRIPTION
+    Long description
+.PARAMETER abc
+    Parameter description (if any)
+.INPUTS
+    Inputs (if any)
+.OUTPUTS
+    Output (if any)
+.EXAMPLE
+    PS C:\> <example usage>
+    Explanation of what the example does
 .NOTES
+    General notes
     http://www.theservergeeks.com/how-todisk-cleanup-using-powershell/
     https://stackoverflow.com/questions/28852786/automate-process-of-disk-cleanup-cleanmgr-exe-without-user-intervention
+    https://github.com/adbertram/Random-PowerShell-Work/blob/master/File-Folder%20Management/Invoke-WindowsDiskCleanup.ps1
+========================================================================= #>
 
-#>
-
-#$HKLM = [UInt32] "0x80000002"
 $keyPath = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches'
 
 $subKeys = Get-ChildItem -Path $keyPath
 
+# "StateFlags0001" AND "StateFlags0065" DON'T APPEAR TO BE USED IN WINDOWS 10
+# I'M GUESSING THEY WERE FROM PERVIOUS VERSIONS OF WINDOWS?
 $regParams = @{
-    Name         = 'StateFlags' #"StateFlags0065" #"StateFlags0001"
-    PropertyType = 'DWORD'
-    Value        = 2
+    Name         = 'StateFlags'
+    PropertyType = 'DWord'
+    Value        = 2 # THIS MAY NEED TO BE A VALUE OF 1
     ErrorAction  = 'SilentlyContinue'
 }
 
@@ -31,17 +46,19 @@ foreach ($sk in $subKeys) {
 }
 
 # RUN CLEANUP MANAGER
-$cleanParams = @{
+$diskCleanup = @{
     FilePath      = "cleanmgr.exe"
-    ArgumentList  = "/sagerun:65"
+    ArgumentList  = "/sagerun:65" # BERTRAM IS USING "/sagerun:1" SO I'LL NEED TO LOOKUP THE VALUE DEFINITIONS
     Wait          = $true
     NoNewWindow   = $true
     #ErrorAction   = SilentlyContinue
     #WarningAction = SilentlyContinue
 }
-Start-Process @cleanParams
+Start-Process @diskCleanup
 
 # REMOVE PROPERTY FOR AUTOMATICALLY RUNNING CLEANUP MANAGER
 foreach ($sk in $subKeys) {
     Remove-ItemProperty -Path $keyPath\$sk -Name $regParams.Name #| Out-Null
 }
+
+# Get-Process -Name cleanmgr, dismhost -ErrorAction SilentlyContinue | Wait-Process
