eb0daaa5d21be473ed9137296db4ffd263a0d237
Justin Johns <just9539@esri.com>
2020-08-11 09:50:44 -0700

    Code clean

--- SecurityTools.psm1
+++ SecurityTools.psm1
@@ -1,10 +1,6 @@
 # IMPORT ALL FUNCTIONS
 foreach ( $directory in @('Public', 'Private') ) {
     foreach ( $fn in (Get-ChildItem -Path "$PSScriptRoot\$directory\*.ps1") ) { . $fn.FullName }
-    <# foreach ( $fn in (Get-ChildItem -Path "$PSScriptRoot\$directory\*.ps1") ) {
-        try { . $fn.FullName }
-        catch { Write-Error -Message ('Failed to load function [{0}] with error: {1}' -f $fn.Name, $_.Exception.Message) }
-    } #>
 }
 
 # VARIABLES
