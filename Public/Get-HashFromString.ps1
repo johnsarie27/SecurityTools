5bf92edfd066836c21bcd3260083f01de573fbb2
Justin Johns <johnsarie@gmail.com>
2020-09-25 21:47:00 -0700

    Added file Get-HashFromString.ps1 added function to manifest

---
+++ Public/Get-HashFromString.ps1
@@ -0,0 +1,56 @@
+function Get-HashFromString {
+    <# =========================================================================
+    .SYNOPSIS
+        Generate hash
+    .DESCRIPTION
+        Generate hash from string input
+    .PARAMETER String
+        String to has
+    .PARAMETER Algorithm
+        Algorithm used to generate hash
+    .INPUTS
+        System.String.
+    .OUTPUTS
+        System.String.
+    .EXAMPLE
+        PS C:\> Get-HashFromString -String 'String' -Algorithm SHA256
+        Generate the SHA256 hash value of "String"
+    .NOTES
+        General notes
+    ========================================================================= #>
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory, ValueFromPipeline, HelpMessage = 'String to hash')]
+        [string] $String,
+
+        [Parameter(HelpMessage = 'Hashing algorithm to use')]
+        [ValidateSet('MD5', 'SHA1', 'SHA256', 'SHA512')]
+        [string] $Algorithm = 'MD5'
+    )
+
+    Process {
+        # THE TYPE COULD BE CHANGED FROM ASCII TO UTF8 OR UNICODE
+        # DEPENDING ON THE STRING INPUT
+        $inputBytes = [System.Text.Encoding]::ASCII.GetBytes($String)
+        Write-Verbose -Message ('Using {0} encoding' -f 'ASCII')
+
+        $ag = switch ($Algorithm) {
+            'MD5' { [System.Security.Cryptography.MD5]::Create() }
+            'SHA1' { [System.Security.Cryptography.SHA1]::Create() }
+            'SHA256' { [System.Security.Cryptography.SHA256]::Create() }
+            'SHA512' { [System.Security.Cryptography.SHA512]::Create() }
+        }
+        Write-Verbose -Message ('Using algorithm {0}' -f $Algorithm)
+
+        $stringBuilder = New-Object System.Text.StringBuilder
+
+        Write-Verbose -Message 'Computing hash...'
+        $ag.ComputeHash($inputBytes) | ForEach-Object -Process {
+            # APPENDING TO THE STRING BUILDER PRINTS TO THE CONSOLE
+            # THE VOID REMOVES THAT CONSOLE OUTPUT
+            [void] $stringBuilder.Append($_.ToString("x2"))
+        }
+
+        $StringBuilder.ToString()
+    }
+}
--- SecurityTools.psd1
+++ SecurityTools.psd1
@@ -9,7 +9,7 @@
     RootModule        = 'SecurityTools.psm1'
 
     # Version number of this module.
-    ModuleVersion     = '0.6.11'
+    ModuleVersion     = '0.6.12'
 
     # Supported PSEditions
     # CompatiblePSEditions = @()
@@ -92,6 +92,7 @@
         'Get-CVSSv3BaseScore'
         'Get-DirItemAges'
         'Get-DirStats'
+        'Get-HashFromString'
         'Get-Ipinfo'
         'Get-LoggedOnUser'
         'Get-MsiInfo'
