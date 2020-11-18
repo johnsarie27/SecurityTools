4201f560b7d6e79cf1bca7f4c9013570223fd1f2
Justin Johns <johnsarie@gmail.com>
2020-11-17 20:57:00 -0800

    Added SupportsShouldProcess to function

--- Public/Remove-Signature.ps1
+++ Public/Remove-Signature.ps1
@@ -16,7 +16,7 @@ function Remove-Signature {
     .NOTES
         General notes
     ========================================================================= #>
-    [CmdletBinding()]
+    [CmdletBinding(SupportsShouldProcess = $true, ConfirmImpact = 'High')]
     Param(
         [Parameter(Mandatory, Position = 0, ValueFromPipeline, HelpMessage = 'Path to script file')]
         [ValidateScript({ Test-Path -Path $_ -PathType Leaf -Include "*.ps*" })]
@@ -29,7 +29,9 @@ function Remove-Signature {
             $content = Get-Content $file
             $line = ($content | Select-String "SIG # Begin signature block").LineNumber
             if ($line) {
-                $content[0..($line - 3)] | Set-Content -Path $file
+                if ($PSCmdlet.ShouldProcess("$file", "Removing signature block")) {
+                    $content[0..($line - 3)] | Set-Content -Path $file
+                }
                 Write-Output -InputObject ('Removed signature from file: {0}' -f $file)
             }
             else {
