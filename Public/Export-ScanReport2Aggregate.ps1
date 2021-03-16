ac58102bb26bc56eeabd8e08bc44d50b9a122d13
Justin Johns <jjohns@esri.com>
2021-03-16 15:56:11 -0700

    Added authenticated scan to Export-ScanReport2Aggregate

--- Public/Export-ScanReport2Aggregate.ps1
+++ Public/Export-ScanReport2Aggregate.ps1
@@ -6,14 +6,16 @@ function Export-ScanReport2Aggregate {
         This function takes a web scan and system scan report and applies
         standard filters and columns to each for easy review in a single
         spreadsheet with multiple tabs.
-    .PARAMETER SystemScan
+    .PARAMETER OutputDirectory
+        Path to output directory
+    .PARAMETER NessusScan
         Path to System scan as a sting
     .PARAMETER WebScan
         Path to Web scan as a string
     .PARAMETER DatabaseScan
         Path to Database scan as a string
-    .PARAMETER OutputDirectory
-        Path to output directory
+    .PARAMETER AcunetixScan
+        Path to Authenticated Web scan as a string
     .INPUTS
         System.String. Format-ScanResults accepts string values for SystemScan
         and WebScan parameters.
@@ -31,26 +33,21 @@ function Export-ScanReport2Aggregate {
         [Alias('DestinationPath')]
         [string] $OutputDirectory = "$HOME\Desktop",
 
-        [Parameter(Mandatory, ParameterSetName = 'sys', HelpMessage = 'CSV file for system scan report')]
-        [Parameter(Mandatory, ParameterSetName = 'sysweb')]
-        [Parameter(Mandatory, ParameterSetName = 'sysdb')]
-        [Parameter(Mandatory, ParameterSetName = 'all')]
+        [Parameter(ParameterSetName = '__sys', HelpMessage = 'CSV file for system scan report')]
         [ValidateScript({ Test-Path -Path $_ -PathType Leaf -Include "*.csv" })]
-        [string] $SystemScan,
+        [string] $NessusScan,
 
-        [Parameter(Mandatory, ParameterSetName = 'web', HelpMessage = 'CSV file for web scan report')]
-        [Parameter(Mandatory, ParameterSetName = 'sysweb')]
-        [Parameter(Mandatory, ParameterSetName = 'webdb')]
-        [Parameter(Mandatory, ParameterSetName = 'all')]
-        [ValidateScript( { Test-Path -Path $_ -PathType Leaf -Include "*.csv" })]
+        [Parameter(ParameterSetName = '__web', HelpMessage = 'CSV file for web scan report')]
+        [ValidateScript({ Test-Path -Path $_ -PathType Leaf -Include "*.csv" })]
         [string] $WebScan,
 
-        [Parameter(Mandatory, ParameterSetName = 'db', HelpMessage = 'CSV file for DB scan report')]
-        [Parameter(Mandatory, ParameterSetName = 'sysdb')]
-        [Parameter(Mandatory, ParameterSetName = 'webdb')]
-        [Parameter(Mandatory, ParameterSetName = 'all')]
-        [ValidateScript( { Test-Path -Path $_ -PathType Leaf -Include "*.xlsx" })]
-        [string] $DatabaseScan
+        [Parameter(ParameterSetName = '__dbs', HelpMessage = 'CSV file for DB scan report')]
+        [ValidateScript({ Test-Path -Path $_ -PathType Leaf -Include "*.xlsx" })]
+        [string] $DatabaseScan,
+
+        [Parameter(ParameterSetName = '__aut', HelpMessage = 'CSV file for authenticated web scan report')]
+        [ValidateScript({ Test-Path -Path $_ -PathType Leaf -Include "*.csv" })]
+        [string] $AcunetixScan
     )
 
     Begin {
@@ -59,21 +56,41 @@ function Export-ScanReport2Aggregate {
 
         # SET VARS
         $webProps = @(
-            "IP-address"
-            "Detected Hostname"
-            "Port number"
-            "Exposure ID"
-            "Name"
-            "Protocol"
-            "Service Description"
-            "Severity"
-            "CVSS"
-            "Description"
-            "Impact"
-            "Resolution"
-            "Evidence"
-            "References"
+            'IP-address'
+            'Detected Hostname'
+            'Port number'
+            'Exposure ID'
+            'Name'
+            'Protocol'
+            'Service Description'
+            'Severity'
+            'CVSS'
+            'Description'
+            'Impact'
+            'Resolution'
+            'Evidence'
+            'References'
             @{ Name = 'Status'; Expression = { $_.'Active or inactive' } }
+        )
+
+        $authWebProps = @(
+            'Name'
+            'ModuleName'
+            'Details'
+            'Parameter'
+            'IsFalsePositive'
+            'Severity'
+            'Type'
+            'Impact'
+            'Description'
+            'Detailed Information'
+            'Recommendation'
+            'Request'
+            'CWEList'
+            'CVEList'
+            'CVSS Score'
+            'CVSS3 Score'
+            @{ Name = 'Reference'; Expression = { $_.'Reference (Name|Url' } }
         )
 
         # SET REPORT OPTIONS
@@ -85,6 +102,10 @@ function Export-ScanReport2Aggregate {
             MoveToEnd    = $true
             Style        = (New-ExcelStyle -Bold -Range '1:1' -HorizontalAlignment Center)
             Path         = Join-Path -Path $OutputDirectory -ChildPath ('Aggregate-Scans_{0:yyyy-MM}.xlsx' -f (Get-Date))
+        }
+
+        if ( $PSCmdlet.ParameterSetName -notin @('__sys', '__dbs'. '__web'.'__aut') ) {
+            Throw 'Please enter parameters for report aggregation'
         }
     }
 
@@ -98,9 +119,18 @@ function Export-ScanReport2Aggregate {
             $webCsv | Select-Object -Property $webProps | Export-Excel @splat -WorksheetName 'WebScan'
         }
 
+        # PROCESS AUTHENTICATED WEB SCAN DATA
+        if ( $PSBoundParameters.ContainsKey('AuthenticatedWebScan') ) {
+            $authCsv = Import-Csv -Path $AcunetixScan
+            #foreach ( $i in $authCsv ) { $i | Add-Member -MemberType NoteProperty -Name 'Source' -Value 'Web Scan' }
+
+            # EXPORT AUTHENTICATED WEB DATA TO EXCEL
+            $authCsv | Select-Object -Property $authWebProps | Export-Excel @splat -WorksheetName 'Acunetix'
+        }
+
         # PROCESS SYSTEM SCAN DATA
         if ( $PSBoundParameters.ContainsKey('SystemScan') ) {
-            $systemCsv = Import-Csv -Path $SystemScan
+            $systemCsv = Import-Csv -Path $NessusScan
             #foreach ( $i in $systemCsv ) { $i | Add-Member -MemberType NoteProperty -Name 'Source' -Value 'System Scan' }
 
             # EXPORT SYSTEM DATA TO EXCEL
