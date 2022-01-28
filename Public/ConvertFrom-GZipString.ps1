57620653212a204066f9f8d2306b2f9ca8da6cfd
Justin Johns <jjohns@esri.com>
2022-01-28 11:54:16 -0800

    code clean

--- Public/ConvertFrom-GZipString.ps1
+++ Public/ConvertFrom-GZipString.ps1
@@ -29,9 +29,10 @@ function ConvertFrom-GZipString {
         [Parameter(Mandatory, ValueFromPipeline)]
         [string[]] $String
     )
+    Begin {
+        Write-Verbose -Message "Starting $($MyInvocation.Mycommand)"
+    }
     Process {
-        Write-Verbose -Message "Starting $(MyInvocation.Mycommand)"
-
         foreach ($str in $String) {
             $compressedBytes = [System.Convert]::FromBase64String($str)
             $ms = New-Object System.IO.MemoryStream
@@ -40,7 +41,12 @@ function ConvertFrom-GZipString {
             $cs = New-Object System.IO.Compression.GZipStream($ms, [System.IO.Compression.CompressionMode]::Decompress)
             $sr = New-Object System.IO.StreamReader($cs)
             $sr.ReadToEnd()
-            $ms.Dispose(); $cs.Dispose(); $sr.Dispose()
+
         }
+    }
+    End {
+        $ms.Dispose()
+        $cs.Dispose()
+        $sr.Dispose()
     }
 }
