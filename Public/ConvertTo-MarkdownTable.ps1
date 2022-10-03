ca75f608df0e059d33c9615d3860d0fa86c740e3
Justin Johns <johnsarie@gmail.com>
2022-10-02 18:30:41 -0700

    Updated output

--- Public/ConvertTo-MarkdownTable.ps1
+++ Public/ConvertTo-MarkdownTable.ps1
@@ -1,25 +1,27 @@
 function ConvertTo-MarkdownTable {
     <# =========================================================================
     .SYNOPSIS
-        Short description
+        Convert array of objects to Markdown table
     .DESCRIPTION
-        Long description
+        Convert array of objects to Markdown table
     .PARAMETER InputObject
-        Input object
+        Input object.
     .INPUTS
         System.Object.
     .OUTPUTS
         None.
     .EXAMPLE
-        PS C:\> <example usage>
-        Explanation of what the example does
+        PS C:\> $svc = Get-Service | Select-Object -Property DisplayName, Name, Description
+        PS C:\> $svc | Select-Object -First 5 | ConvertTo-MarkdownTable
+        Converts the first 5 services to a Markdown table
     .NOTES
         Name:     ConvertTo-MarkDownTable
         Author:   Justin Johns
         Version:  0.1.0 | Last Edit: 2022-09-28
         - 0.1.0 - Initial version
+        - 0.1.1 - Added spaces around text
         Comments: <Comment(s)>
-        General notes
+        General notes:
         https://stackoverflow.com/questions/69010143/convert-powershell-output-to-a-markdown-file
     ========================================================================= #>
     [CmdletBinding()]
@@ -39,9 +41,9 @@ function ConvertTo-MarkdownTable {
             $headersDone = $true
             # output the header line and below that a dashed line
             # -replace '(?<!\\)\|', '\|' escapes every '|' unless already escaped
-            '|{0}|' -f (($_.PSObject.Properties.Name -replace $pattern, '\|') -join '|')
-            '|{0}|' -f (($_.PSObject.Properties.Name -replace '.', '-') -join '|')
+            '| {0} |' -f (($_.PSObject.Properties.Name -replace $pattern, '\|') -join ' | ')
+            '| {0} |' -f (($_.PSObject.Properties.Name -replace '.', '-') -join ' | ')
         }
-        '|{0}|' -f (($_.PsObject.Properties.Value -replace $pattern, '\|') -join '|')
+        '| {0} |' -f (($_.PsObject.Properties.Value -replace $pattern, '\|') -join ' | ')
     }
 }
