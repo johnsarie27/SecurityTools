0d73312e1bb573542fe66aba8429c0bd87740116
Justin Johns <jjohns@esri.com>
2023-10-05 08:37:27 -0700

    Rename file and function (#16)

--- SecurityTools.psm1
+++ SecurityTools.psm1
@@ -16,3 +16,4 @@ New-Variable -Name 'InfoModel' -Option ReadOnly -Value (
 # THESE ARE SPECIFIED IN THE MODULE MANIFEST AND THEREFORE DON'T NEED TO BE LISTED HERE
 #Export-ModuleMember -Function *
 Export-ModuleMember -Variable 'EventTable', 'InfoModel'
+Export-ModuleMember -Alias 'Get-ActiveGWUser'
