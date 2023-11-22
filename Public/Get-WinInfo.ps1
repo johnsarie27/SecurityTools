af921cead46460a703f5504037e634b33649f8de
jjohns <jjohns@esri.com>
2023-11-22 08:50:49 -0800

    Update CIM and WinEventLogs functions updates to make both functions work the same way fixed issue with list parameter removed ID from InformationModel data

--- Public/Get-WinInfo.ps1
+++ Public/Get-WinInfo.ps1
@@ -18,7 +18,14 @@ function Get-WinInfo {
         PS C:\> Get-WinInfo -List
         List available classes to pull information from
     .NOTES
+        Name:     Get-WinInfo
+        Author:   Justin Johns
+        Version:  0.1.1 | Last Edit: 2023-11-22
+        - 0.1.1 - Usability updates
+        - 0.1.0 - Initial version
+        Comments: <Comment(s)>
         General notes
+        https://learn.microsoft.com/en-us/powershell/module/cimcmdlets/get-ciminstance
     ========================================================================= #>
     [CmdletBinding(DefaultParameterSetName = '__list')]
     Param(
@@ -27,7 +34,7 @@ function Get-WinInfo {
 
         [Parameter(Mandatory, HelpMessage = 'Class Id', ParameterSetName = '__info')]
         [ValidateNotNullOrEmpty()]
-        [ValidateScript({ $InfoModel.Classes.Id -contains $_ })]
+        [ValidateScript({ $_ -GT 0 -and $_ -LE $InfoModel.Classes.Count })]
         [System.Int32] $Id,
 
         [Parameter(ValueFromPipeline, HelpMessage = 'Hostname of target computer', ParameterSetName = '__info')]
@@ -36,16 +43,28 @@ function Get-WinInfo {
         [Alias('CN')]
         [System.String] $ComputerName
     )
+    Begin {
+        Write-Verbose -Message "Starting $($MyInvocation.Mycommand)"
 
+        # CREATE EVENT LIST
+        $infoList = for ($i = 0; $i -LT $InfoModel.Classes.Count; $i++) {
+            [PSCustomObject] @{
+                Id        = ($i + 1)
+                Namespace = $InfoModel.Classes[$i].Namespace
+                ClassName = $InfoModel.Classes[$i].ClassName
+                Filters   = $InfoModel.Classes[$i].Filters
+                Comments  = $InfoModel.Classes[$i].Comments
+            }
+        }
+    }
     Process {
-        $infoModel = Get-Content -Raw -Path "$PSScriptRoot\InformationModel.json" | ConvertFrom-Json
-
+        # LIST OR GET INFORMATION
         switch ($PSCmdlet.ParameterSetName) {
             '__list' {
-                $infoModel.Classes | Select-Object -Property Id, Comments
+                $infoList | Format-Table -AutoSize
             }
             '__info' {
-                $im = $infoModel.Classes.Where({ $_.Id -EQ $Id })
+                $im = $InfoModel.Classes[($Id - 1)]
 
                 $cimParams = @{
                     Namespace = $im.Namespace
--- SecurityTools.psd1
+++ SecurityTools.psd1
@@ -149,7 +149,7 @@
     # Variables to export from this module
     VariablesToExport = @(
         'EventTable'
-        'InformationModel'
+        'InfoModel'
     )
 
     # Aliases to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no aliases to export.
