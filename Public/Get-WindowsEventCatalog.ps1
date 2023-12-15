1f3288671e095c4f19c5798fca16f2ea87059c15
jjohns <jjohns@esri.com>
2023-12-15 08:56:38 -0800

    updated function for data location change

--- Public/Get-WindowsEventCatalog.ps1
+++ Public/Get-WindowsEventCatalog.ps1
@@ -14,8 +14,9 @@ function Get-WindowsEventCatalog {
     .NOTES
         Name:     Get-WindowsEventCatalog
         Author:   Justin Johns
-        Version:  0.1.0 | Last Edit: 2022-11-16
-        - 0.1.0 - Initial version
+        Version:  0.1.1 | Last Edit: 2023-12-15
+        - 0.1.1 - Updates for data location change
+        - 0.1.0 - (2022-11-16) Initial version
         Comments: <Comment(s)>
         General notes
     #>
@@ -24,10 +25,13 @@ function Get-WindowsEventCatalog {
     Begin {
         Write-Verbose -Message "Starting $($MyInvocation.Mycommand)"
 
-        # SET URI
-        $uri = 'https://gist.githubusercontent.com/johnsarie27/5519dd08bae06b8b6271ac168e28e06a/raw/321c3a46756beb021012df4f2e26cccbd7fe6417/windows_signatures_850.csv'
+        # SET PATH
+        #$uri = 'https://gist.githubusercontent.com/johnsarie27/5519dd08bae06b8b6271ac168e28e06a/raw/321c3a46756beb021012df4f2e26cccbd7fe6417/windows_signatures_850.csv'
+        $path = "$PSScriptRoot\..\Private\windows_signatures_850.csv"
     }
     Process {
-        Invoke-RestMethod -Uri $uri | ConvertFrom-Csv
+        # GET DATA
+        #Invoke-RestMethod -Uri $uri | ConvertFrom-Csv
+        Import-Csv -Path $path
     }
 }
