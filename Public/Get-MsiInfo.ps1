f99f0d4fd569c5a8860f5ec7fbb3ceace5db96a6
jjohns <jjohns@esri.com>
2024-08-13 12:03:11 -0700

    Update Get-MsiInfo function

--- Public/Get-MsiInfo.ps1
+++ Public/Get-MsiInfo.ps1
@@ -3,53 +3,66 @@ function Get-MsiInfo {
     .SYNOPSIS
         Get MSI information
     .DESCRIPTION
-        Get specific MSI property information
+        Get information from MSI file
     .PARAMETER Path
         Path to MSI file
-    .PARAMETER Property
-        Desired property to obtain from MSI Database
     .INPUTS
         None.
     .OUTPUTS
-        None.
+        System.Object.
     .EXAMPLE
-        PS C:\> Get-MsiInfo -Path "C:\myMsi.msi" -Property ProductCode
-        Returns the product code for myMsi.msi
+        PS C:\> Get-MsiInfo -Path "C:\myMsi.msi"
+        Returns all available product info for myMsi.msi
     .NOTES
         General notes
         This function was originally written by Nickolaj Andersen (see link below)
         https://www.scconfigmgr.com/2014/08/22/how-to-get-msi-file-information-with-powershell/
+        https://pscode.dev/get-msifileinfo
     #>
     [CmdletBinding()]
     Param(
-        [parameter(Mandatory = $true)]
+        [parameter(Mandatory = $true, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
         [ValidateNotNullOrEmpty()]
-        [System.IO.FileInfo] $Path,
-
-        [parameter(Mandatory = $true)]
-        [ValidateNotNullOrEmpty()]
-        [ValidateSet("ProductCode", "ProductVersion", "ProductName", "Manufacturer", "ProductLanguage", "FullVersion")]
-        [System.String] $Property
+        [System.IO.FileInfo] $Path
     )
+    Begin {
+        Write-Verbose -Message "Starting $($MyInvocation.Mycommand)"
+    }
     Process {
         try {
             # READ PROPERTY FROM MSI DATABASE
-            $windowsInstaller = New-Object -ComObject WindowsInstaller.Installer
-            $msiDatabase = $windowsInstaller.GetType().InvokeMember("OpenDatabase", "InvokeMethod", $null, $windowsInstaller, @($Path.FullName, 0))
-            $query = "SELECT Value FROM Property WHERE Property = '$($Property)'"
-            $view = $msiDatabase.GetType().InvokeMember("OpenView", "InvokeMethod", $null, $msiDatabase, ($query))
-            $view.GetType().InvokeMember("Execute", "InvokeMethod", $null, $view, $null)
-            $record = $view.GetType().InvokeMember("Fetch", "InvokeMethod", $null, $view, $null)
-            $value = $record.GetType().InvokeMember("StringData", "GetProperty", $null, $record, 1)
+            if (-Not ([System.IO.Path]::IsPathRooted($Path))) {
+                $MsiPath = (Resolve-Path -Path $Path).Path -As [System.IO.FileInfo]
+            }
+            else {
+                $MsiPath = (Get-ChildItem -Path $Path) -As [System.IO.FileInfo]
+            }
+            if ($MsiPath.Extension -ne ".msi") {
+                Write-Error -Message 'Path must be an msi file.' -ErrorAction Stop
+            }
 
-            # COMMIT DATABASE AND CLOSE VIEW
-            $msiDatabase.GetType().InvokeMember("Commit", "InvokeMethod", $null, $msiDatabase, $null)
-            $view.GetType().InvokeMember("Close", "InvokeMethod", $null, $view, $null)
-            $msiDatabase = $null
-            $view = $null
+            $MsiValue = @{}
+            Write-Verbose -Message 'Open and Read property from MSI database'
+            $MsiObj = New-Object -ComObject WindowsInstaller.Installer
+            $MsiDb = $MsiObj.GetType().InvokeMember('OpenDatabase', 'InvokeMethod', $null, $MsiObj, @($MsiPath.FullName, 0))
+            $View = $MsiDb.GetType().InvokeMember('OpenView', 'InvokeMethod', $null, $MsiDb, ("SELECT * FROM Property"))
+            $View.GetType().InvokeMember('Execute', 'InvokeMethod', $null, $View, $null)
+            While ($Record = $View.GetType().InvokeMember('Fetch', 'InvokeMethod', $null, $View, $null)) {
+                $Name = $Record.GetType().InvokeMember('StringData', 'GetProperty', $null, $Record, 1)
+                $Value = $Record.GetType().InvokeMember('StringData', 'GetProperty', $null, $Record, 2)
+                $MsiValue.Add($Name, $Value)
+            }
+            $MsiValue.Add('FileName', $MsiPath.Name)
+            $MsiValue.Add('FilePath', $MsiPath.FullName)
+
+            Write-Verbose "Commit database and close view"
+            $MsiDb.GetType().InvokeMember('Commit', 'InvokeMethod', $null, $MsiDb, $null)
+            $View.GetType().InvokeMember('Close', 'InvokeMethod', $null, $View, $null)
+            $MsiDb = $null
+            $View = $null
 
             # RETURN THE VALUE
-            return $value
+            return ([PSCustomObject] $MsiValue)
         }
         catch {
             $PSCmdlet.ThrowTerminatingError($PSItem)
@@ -57,7 +70,8 @@ function Get-MsiInfo {
     }
     End {
         # RUN GARBAGE COLLECTION AND RELEASE COMOBJECT
-        [System.Runtime.Interopservices.Marshal]::ReleaseComObject($windowsInstaller) | Out-Null
+        [System.Runtime.Interopservices.Marshal]::ReleaseComObject($MsiObj) | Out-Null
+        $MsiObj = $null
         [System.GC]::Collect()
     }
 }
