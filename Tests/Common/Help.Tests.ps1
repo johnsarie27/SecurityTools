84e1c1cb204bcd62cd2b1674b6c015309f4f0773
jjohns <jjohns@esri.com>
2024-10-10 12:59:54 -0700

    fixed issue with build automation

--- Tests/Common/Help.Tests.ps1
+++ Tests/Common/Help.Tests.ps1
@@ -1,8 +1,8 @@
 BeforeDiscovery {
     # Taken with love from @juneb_get_help (https://raw.githubusercontent.com/juneb/PesterTDD/master/Module.Help.Tests.ps1)
     # Import module
-    if (-not (Get-Module -Name $env:BHProjectName -ListAvailable)) {
-        Import-Module -Name $env:BHPSModuleManifest -ErrorAction 'Stop' -Force
+    if (-not (Get-Module -Name $env:BHProjectName <# -ListAvailable #>)) {
+        Import-Module -Name $env:BHPSModuleManifest -ErrorAction 'Stop' #-Force
     }
     $Cmdlets = Get-Command -Module $env:BHProjectName -CommandType 'Cmdlet', 'Function' -ErrorAction 'Stop'
 }
