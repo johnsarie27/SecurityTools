46147f18d30995cfdde4a159f7486bdf68a18333
Justin Johns <jjohns@esri.com>
2025-06-13 15:36:35 -0700

    update Expand-GZip parameter DestinationPath to support directory

--- Public/Expand-GZip.ps1
+++ Public/Expand-GZip.ps1
@@ -18,28 +18,43 @@ function Expand-GZip {
     .NOTES
         Name:     Expand-GZip
         Author:   RiffyRiot
-        Version:  0.1.0 | Last Edit: 2023-10-30
-        - 0.1.0 - Initial version
-        Comments: <Comment(s)>
-        General notes:
+        Version:  0.1.1 | Last Edit: 2025-06-13
+        - see commit history for version history
+        General Notes:
         https://social.technet.microsoft.com/Forums/windowsserver/en-US/5aa53fef-5229-4313-a035-8b3a38ab93f5/unzip-gz-files-using-powershell?forum=winserverpowershell
     #>
     [CmdletBinding()]
     Param(
         [Parameter(Mandatory = $true, Position = 0, HelpMessage = 'Path to GZip file')]
-        [ValidateScript({ Test-Path -Path $_ -PathType Leaf -Include '*.gz*' })]
-        [System.String] $Path,
+        [ValidateScript({ Test-Path -Path $_ -PathType Leaf -Include '*.gz' })]
+        [System.IO.FileInfo] $Path,
 
         [Parameter(Mandatory = $false, Position = 1, HelpMessage = 'Destination path to extract file to')]
-        [ValidateScript({ Test-Path -Path (Split-Path -Path $_) -PathType Container })]
-        [System.String] $DestinationPath = ($Path -replace '\.gz$', '')
+        [ValidateScript({ Test-Path -Path $_ -PathType Container })]
+        [System.IO.FileInfo] $DestinationPath
     )
     Begin {
         Write-Verbose -Message "Starting $($MyInvocation.Mycommand)"
     }
     Process {
+        # CHECK FOR DESTINATION PATH
+        if ($PSBoundParameters.ContainsKey('DestinationPath')) {
+            # SET DESTINATION TO FULL, DECOMPRESSED FILE PATH
+            #$destFullPath = Join-Path -Path $DestinationPath -ChildPath ([System.IO.Path]::GetFileNameWithoutExtension($Path))
+            $destFullPath = Join-Path -Path $DestinationPath -ChildPath (Split-Path -Path $Path -LeafBase)
+        }
+        else {
+            # IF NO DESTINATION PATH IS PROVIDED, USE THE SAME PATH AS THE GZIP FILE BUT WITHOUT THE .GZ EXTENSION
+            #$destFullPath = $Path -replace '\.gz$', ''
+            $destFullPath = Join-Path -Path (Split-Path -Path $Path) -ChildPath (Split-Path -Path $Path -LeafBase)
+        }
+
+        # OUTPUT VERBOSE MESSAGE
+        Write-Verbose -Message ('Expanding GZip file [{0}] to [{1}]' -f $Path, $destFullPath)
+
+        # CREATE .NET OBJECTS
         $ip = New-Object System.IO.FileStream $Path, ([IO.FileMode]::Open), ([IO.FileAccess]::Read), ([IO.FileShare]::Read)
-        $op = New-Object System.IO.FileStream $DestinationPath, ([IO.FileMode]::Create), ([IO.FileAccess]::Write), ([IO.FileShare]::None)
+        $op = New-Object System.IO.FileStream $destFullPath, ([IO.FileMode]::Create), ([IO.FileAccess]::Write), ([IO.FileShare]::None)
         $gzipStream = New-Object System.IO.Compression.GzipStream $ip, ([IO.Compression.CompressionMode]::Decompress)
 
         $buffer = New-Object byte[](1024)
