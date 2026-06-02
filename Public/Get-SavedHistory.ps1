5c156395b0d639170dac7f1bec39c86aee2602c4
Justin Johns <johnsarie@gmail.com>
2026-06-01 21:56:05 -0700

    fix(Public): refactor Get-SavedHistory loop
    - Move notmatch filters into Begin block alongside other filtering
    - Replace hybrid foreach/counter with a clean for loop in Process

--- Public/Get-SavedHistory.ps1
+++ Public/Get-SavedHistory.ps1
@@ -27,18 +27,15 @@ function Get-SavedHistory {
         [System.String] $Search
     )
     Begin {
-        $history = Get-Content (Get-PSReadLineOption).HistorySavePath |
-            Where-Object { $_ -like "*$Search*" } | Select-Object -Unique
+        $where = { $_ -like "*$Search*" -and $_ -notmatch 'HistorySavePath' -and $_ -notmatch 'SavedHistory' }
+        $history = Get-Content -Path (Get-PSReadLineOption).HistorySavePath |
+            Where-Object -FilterScript $where | Select-Object -Unique
     }
     Process {
-        $i = 1
-        foreach ( $line in $history ) {
-            if ( $line -notmatch 'HistorySavePath' -and $line -notmatch 'SavedHistory' ) {
-                [PSCustomObject] @{
-                    Id          = $i
-                    CommandLine = $line
-                }
-                $i++
+        for ($i = 0; $i -lt $history.Count; $i++) {
+            [PSCustomObject] @{
+                Id          = ($i + 1)
+                CommandLine = $history[$i]
             }
         }
     }
